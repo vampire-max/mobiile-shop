8163fa77ecdf3779dfea3a254f39f3f0ea91ba51
jana6600 <jananikolic6600@gmail.com>
2021-10-21 00:40:35 -0700

    hompage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Roboto': ['Roboto', 'sans-serif']
+      },
+
+      colors: {
+        gray: {
+          300: "#C9C9C9",
+          500: "#515C6F",
+          700: "#222953",
+          800: "#161D46",
+          dark: "#232323",
+        },
+
+        yellow: {
+          DEFAULT: "#FF9900",
+        },
+
+        blue: {
+          DEFAULT: "#67B5F7",
+        },
+
+        purple: {
+          DEFAULT: "#521AB2",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
